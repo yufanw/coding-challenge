b1e127ced38fba05f2bc69d53ea8ab3e91da0bd6
Yufan Wang <yufanwangcn@hotmail.com>
2018-07-10 16:58:43 -0700

    cleaning up component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import Picker from "./Picker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Picker />, document.getElementById("root"));
